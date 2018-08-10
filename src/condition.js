bf1b8ad305c9b95cadbd57960d288564211dbc0e
Abel Trotter <a_trotter0@hotmail.com>
2018-08-10 10:08:03 -0700

    Add buildConditions method, add loadConditions and addToSelect functions in main

--- src/condition.js
+++ src/condition.js
@@ -1,5 +1,9 @@
 export class Condition {
-  static allConditions() {
+  constructor() {
+    this.conditionsList = [];
+  }
+
+  getConditions() {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
       let url = `https://api.betterdoctor.com/2016-03-01/conditions?&user_key=${process.env.exports.apiKey}`;
@@ -10,8 +14,16 @@ export class Condition {
           reject(Error(request.statusText));
         }
       }
-      request.open("GET", url, true);
+      request.open('GET', url, true);
       request.send();
     });
+  }
+  
+  buildConditionsList(apiResponse) {
+    const parsedResponse = JSON.parse(apiResponse);
+    parsedResponse.data.forEach((condition) => {
+      this.conditionsList.push(condition.name);
+    });
+    this.conditionsList.sort();
   }
 }
--- src/main.js
+++ src/main.js
@@ -2,7 +2,31 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { Condition } from './condition';
+
+function loadConditions() {
+  const condition = new Condition();
+  let allConditionsPromise = condition.getConditions();
+
+  allConditionsPromise.then((response) => {
+    condition.buildConditionsList(response);
+    console.log(condition.conditionsList);
+    addToSelect(condition.conditionsList);
+  }, (error) => {
+    console.log(error.message);
+  });
+}
+
+function addToSelect(conditions) {
+  conditions.forEach((condition) => {
+    $('#conditions-select').append(`<option value='${condition}'>${condition}</option>`);
+  });
+}
 
 $(document).ready(function() {
+  loadConditions();
 
+  $('search-conditions').click(function() {
+    console.log('clicked search');
+  });
 });
