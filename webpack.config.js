18ac4a9d1c0b64b0802dab8d2edd50ed368475b5
Abel Trotter <a_trotter0@hotmail.com>
2018-08-11 12:26:58 -0700

    Prep for Heroku deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+//const CleanWebpackPlugin = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
@@ -17,9 +17,9 @@ module.exports = {
   plugins: [
     new Dotenv(),
     new UglifyJsPlugin({ sourceMap: true }),
-    new CleanWebpackPlugin(['dist']),
+    //new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'Journal Assignment',
+      title: 'PDX Doctor Lookup',
       template: './src/index.html',
       inject: 'body'
     })
